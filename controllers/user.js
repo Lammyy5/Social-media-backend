9f8aa719d6e3c1b1aa03e0422a3960dbc291fdaa
Lammyy5 <reliford2010@gmail.com>
2023-12-13 07:25:00 -0800

    Added require for user model

--- controllers/user.js
+++ controllers/user.js
@@ -1,58 +1,45 @@
-const UserController = {
-   
-    getAllUsers(req, res) {
-      User.find({})
-        .then(userData => res.json(userData))
-        .catch(err => res.status(500).json(err));
-    },
-  
-  
-    getUserById(req, res) {
-      User.findById(req.params.userId)
-        .then(userData => res.json(userData))
-        .catch(err => res.status(500).json(err));
-    },
-    
-   
-    createUser(req, res) {
-      User.create(req.body)
-        .then(userData => res.json(userData))
-        .catch(err => res.status(500).json(err));
-    },
-  
-    
-    updateUserById(req, res) {
-      User.findOneAndUpdate(req.params.id, req.body, { new: true })
-        .then(userData => {
-          if (!userData) {
-            return res.status(404).json({ message: 'User not found' });
-          }
-          res.json(userData);
-        })
-        .catch(err => res.status(500).json(err));
-    },
-  
-   
-    deleteUserById(req, res) {
-      User.findOneAndDelete(req.params.id)
-        .then(userData => {
-          if (!userData) {
-            return res.status(404).json({ message: 'User not found' });
-          }
-          res.json({ message: 'User deleted successfully' });
-        })
-        .catch(err => res.status(500).json(err));
-    },
-  
-   
-   
+const { User } = require('../models')
 
-  
-  
-    
-  };
-  
-  
- 
-  module.exports = UserController;
-  
+const UserController = {
+	getAllUsers(req, res) {
+		User.find({})
+			.then((userData) => res.json(userData))
+			.catch((err) => res.status(500).json(err))
+	},
+
+	getUserById(req, res) {
+		User.findById(req.params.userId)
+			.then((userData) => res.json(userData))
+			.catch((err) => res.status(500).json(err))
+	},
+
+	createUser(req, res) {
+		User.create(req.body)
+			.then((userData) => res.json(userData))
+			.catch((err) => res.status(500).json(err))
+	},
+
+	updateUserById(req, res) {
+		User.findOneAndUpdate(req.params.id, req.body, { new: true })
+			.then((userData) => {
+				if (!userData) {
+					return res.status(404).json({ message: 'User not found' })
+				}
+				res.json(userData)
+			})
+			.catch((err) => res.status(500).json(err))
+	},
+
+	deleteUserById(req, res) {
+		User.findOneAndDelete(req.params.id)
+			.then((userData) => {
+				if (!userData) {
+					return res.status(404).json({ message: 'User not found' })
+				}
+				res.json({ message: 'User deleted successfully' })
+			})
+			.catch((err) => res.status(500).json(err))
+	},
+}
+
+module.exports = UserController
