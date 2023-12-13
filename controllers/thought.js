d593f00bc6e166a95eed1252f46a55e08b04d5c4
Lammyy5 <reliford2010@gmail.com>
2023-12-13 07:23:30 -0800

    Coded thoughts controller

---
+++ controllers/thought.js
@@ -0,0 +1,47 @@
+const { Thought} = require('../models')
+
+const ThoughtController = {
+  async getAllThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find({});
+      res.json(thoughts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  
+  async getThoughtsById(req, res) {
+    try {
+      const thought = await Thought.findOne({_id:req.params.thoughtId});
+      if (!thought) {
+        res.status(404).json({ message: 'Thought not found' });
+      } else {
+        res.json(thought);
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  
+  async createThought(req, res) {
+    try {
+      const thought = await Thought.create(req.body);
+      res.status(201).json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  
+
+  async deleteThought(req,res) {
+    try {
+        const thought = await Thought.findByIdAndDelete({_id:req.params.thoughtId});
+        res.status(200).json(thought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+  },
+}
+
+module.exports = ThoughtController
